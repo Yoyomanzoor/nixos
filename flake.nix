8a66eed519daca59c6d5d9d30925f29e39c4dc47
yoyomanzoor-NixOS <smanzoor@umich.edu>
2024-11-26 23:40:26 -0500

    init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = if builtins ? currentSystem
+        then builtins.currentSystem
+	else "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+    nixosConfigurations = {
+      default = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+	modules = [
+	  ./hosts/default/configuration.nix
+	  # inputs.home-manager.nixosModules.default
+	];
+      };
+    };
+
+  };
+}

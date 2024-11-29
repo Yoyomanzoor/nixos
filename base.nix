c3ebd517a353bbec81837c93b974123b3d437255
yoyomanzoor-NixOS <smanzoor@umich.edu>
2024-11-28 22:32:15 -0600

    some theming things

--- base.nix
+++ base.nix
@@ -160,11 +160,10 @@
   stylix = {
     enable = true;
     autoEnable = true;
-    # targets = {
-    #   fish.enable = true;
-    #   gnome.enable = true;
-    #   grub.enable = true;
-    # };
+    targets = {
+      fish.enable = true;
+      gnome.enable = true;
+    };
 
     image = /home/yoyomanzoor/.config/home-manager/wallpaper.png;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
@@ -183,6 +182,10 @@
       serif = {
         package = pkgs.dejavu_fonts;
         name = "DejaVu Serif";
+      };
+      emoji = {
+        package = pkgs.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
       };
     };
     opacity = {

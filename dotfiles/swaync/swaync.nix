322c13523ce235a1118ef16524a194361c382a03
yoyomanzoor-NixOS <smanzoor@umich.edu>
2024-12-08 19:24:30 -0600

    sway config in home manager

---
+++ dotfiles/swaync/swaync.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+{
+    xdg.configFile."swaync/config.json".source = ./config.json;
+    xdg.configFile."swaync/style.css".source = ./style.css;
+    # xdg.configFile."swaync/themes/nova-dark/notifications.css".source = ./themes/nova-dark/notifications.css;
+    # xdg.configFile."swaync/themes/nova-dark/central_control.css".source = ./themes/nova-dark/central_control.css;
+    xdg.configFile."swaync/themes" = {
+        source = ./themes;
+        recursive = true;
+    };
+    xdg.configFile."swaync/icons" = {
+        source = ./icons;
+        recursive = true;
+    };
+}

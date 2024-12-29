9d03b4661919ef02a346d58194ad176acbefb408
yoyomanzoor-NixOS <smanzoor@umich.edu>
2024-12-29 13:28:39 -0600

    bruh it was that easy to set up kanata

--- base/laptop-base.nix
+++ base/laptop-base.nix
@@ -1,10 +1,11 @@
 {
-# Laptop packages, for things like power profiles
-# See https://nixos.wiki/wiki/Laptop
+  # Laptop packages, for things like power profiles
+  # See https://nixos.wiki/wiki/Laptop
   imports = [
     ./default.nix
   ];
 
+  # Power Management with tlp
   powerManagement.enable = true;
   services.thermald.enable = true;
 
@@ -27,6 +28,40 @@
       #Optional helps save long term battery health
       START_CHARGE_THRESH_BAT0 = 78; # 78 and bellow it starts to charge
       STOP_CHARGE_THRESH_BAT0 = 85; # 85 and above it stops charging
+    };
+  };
+
+  # Keyboard home row mods
+  # https://github.com/dreamsofcode-io/home-row-mods?tab=readme-ov-file
+  # https://github.com/elliottminns/dotfiles/blob/a204380f1b256ad726980fe8694106ef486594e6/nix/configuration.nix#L310-L346
+  services.kanata = {
+    enable = true;
+    keyboards = {
+      internalKeyboard = {
+        extraDefCfg = "process-unmapped-keys yes";
+        config = ''
+          (defsrc
+           a s d f j k l ;
+          )
+          (defvar
+           tap-time 150
+           hold-time 200
+          )
+          (defalias
+           a (tap-hold $tap-time $hold-time a lmet)
+           s (tap-hold $tap-time $hold-time s lalt)
+           d (tap-hold $tap-time $hold-time d lctl)
+           f (tap-hold $tap-time $hold-time f lsft)
+           ; (tap-hold $tap-time $hold-time ; rmet)
+           l (tap-hold $tap-time $hold-time l ralt)
+           k (tap-hold $tap-time $hold-time k rctl)
+           j (tap-hold $tap-time $hold-time j rsft)
+          )
+          (deflayer base
+           @a @s @d @f @j @k @l @;
+          )
+        '';
+      };
     };
   };
 }
